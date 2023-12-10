210d0a08d98796291223ac91bf14409ccee75627
Adewuy <106016379+Adewuy@users.noreply.github.com>
2023-12-10 16:10:28 +0100

    color change

--- src/form.js
+++ src/form.js
@@ -7,7 +7,7 @@ export default function Form() {
     <div class="mx-auto max-w-lg text-center">
       <h1 class="text-2xl font-bold sm:text-3xl">Get started today!</h1>
 
-      <p class="mt-4 text-gray-500">
+      <p class="mt-4 text-blue-500">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Et libero nulla eaque error neque
         ipsa culpa autem, at itaque nostrum!
       </p>

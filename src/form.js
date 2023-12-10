c471008c1f3d36b1a8cc83df27da205097b4c27f
Adewuy <106016379+Adewuy@users.noreply.github.com>
2023-12-10 15:01:56 +0100

    i added h1 that said finally

--- src/form.js
+++ src/form.js
@@ -2,6 +2,7 @@ export default function Form() {
   return (
     <>
       <form>
+        <h1>finally</h1>
         <input type="text" placeholder="username" />
         <input type="text" placeholder="passworrd" />
         <button>submit</button>

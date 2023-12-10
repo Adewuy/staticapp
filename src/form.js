98465d79b9067e4e22558c7c1e87ebfa8b7409bc
Adewuy <106016379+Adewuy@users.noreply.github.com>
2023-12-10 15:18:00 +0100

    i just add dob,  dtails and favorite color

--- src/form.js
+++ src/form.js
@@ -2,10 +2,13 @@ export default function Form() {
   return (
     <>
       <form>
-        <h1>finally</h1>
-        <input type="text" placeholder="username" />
-        <input type="text" placeholder="passworrd" />
+        <h1>Details</h1>
+        <input type="text" placeholder="username" /> <br/>
+        <input type="text" placeholder="passworrd" /> <br/>
+        <input type="text" placeholder="date of birth" /> <br/>
+        <input type="text" placeholder="Favorite color" /> <br/>
         <button>submit</button>
+
       </form>
     </>
   );
